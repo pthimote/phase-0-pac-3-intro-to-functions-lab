fef9a2cc757ac08eded857eca5591e367809533a
Paul Thimote <pthimote@gmail.com>
2022-08-07 14:27:07 -0400

    commit tree 177d44ea586d10acb5f5aa61452740b0db0ae9f5

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  
+  function WHISPER(string) {
+    //todo
+  }
+  function whisper(string) {
+    return (string);
+  }
+  "WHiSPER".toLowerCase(); // 'hello'
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+
+  function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+
+  function logWhisper(string) {
+    console.log(string.toLowerCase());
+  }
+  function sayHiToHeadphonedRoommate(string) {
+    if (string ===string.toLowerCase())
+    return "I can't hear you!"
+
+    if (string ===string.toUpperCase())
+    return "YES INDEED!"
+
+    if (string ==="Let's have dinner together!")
+    return "I would love to!"
+  }
--- test/index-test.js
+++ test/index-test.js
@@ -2,8 +2,8 @@
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO');
-  })
-})
+  });
+});
 
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
